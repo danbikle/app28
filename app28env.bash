26dfaf81c5f3b52b39952fc7e40890147029af51
Dan Bikle <dan.bikle@gmail.com>
2015-11-22 06:17:22 +0000

    more

---
+++ app28env.bash
@@ -0,0 +1,5 @@
+# I should dot this file instead of running it.
+# I use this file to setup my env-variables for ~/ann/app28
+export PATH=${PATH}:/home/ann/app22/node_modules/.bin
+export PATH=/home/ann/heroku-client/bin:${PATH}
+export PATH=/home/ann/node/bin:${PATH}
